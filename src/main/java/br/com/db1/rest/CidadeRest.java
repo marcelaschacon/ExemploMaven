4dbce56cffc215b2bb95391f7b64d15c75ba72d6
marcelaschacon <marcel.alves.lem@hotmail.com>
2018-09-05 17:54:24 -0300

    On web W/ nome

--- src/main/java/br/com/db1/rest/CidadeRest.java
+++ src/main/java/br/com/db1/rest/CidadeRest.java
@@ -2,14 +2,15 @@ package br.com.db1.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("cidade")
 public class CidadeRest {
 
 	@GET
-	@Path("alo")
-	public Response helloWorld(){
-		return Response.status(200).entity("CABE%C7A%20DE%20MARKINHOS").build();
+	@Path("alo/{parametro}")
+	public Response helloWorld(@PathParam("parametro") String nome){
+		return Response.status(200).entity("CABE%C7A%20DE%20MARKINHOS" + nome).build();
 	}
 }
